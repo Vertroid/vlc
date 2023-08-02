dbee55e7dd627bec8ff07b2a439762cd68594668
Vertex <vertex1108@gmail.com>
2023-08-02 15:43:02 +0800

    fix '@#' validations
    (cherry picked from commit 7e12d06cfaf1e8113bf7990c3a035300bdb58f2f)

--- src/input/input.c
+++ src/input/input.c
@@ -3269,9 +3269,12 @@ void input_SplitMRL( const char **access, const char **demux,
                      const char **path, const char **anchor, char *buf )
 {
     char *p;
+    char *proc;
 
     /* Separate <path> from <access>[/<demux>]:// */
     p = strstr( buf, "://" );
+    proc = strstr(buf, "smb://");
+    
     if( p != NULL )
     {
         *p = '\0';
@@ -3281,7 +3284,7 @@ void input_SplitMRL( const char **access, const char **demux,
         /* Remove HTML anchor if present (not supported).
          * The hash symbol itself should be URI-encoded. */
         p = strchr( p, '#' );
-        if( p != NULL )
+        if( p != NULL && proc == NULL)
         {
             *(p++) = '\0';
             *anchor = p;
